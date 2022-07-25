9bc91bbb7a4c75bb50bbfb18dca3c89801624300
V, Somasekhar <Somasekhar.V@cerner.com>
2022-07-25 13:47:06 +0530

    Updated the script

--- src/test/java/learnSelenium/AlertHandles.java
+++ src/test/java/learnSelenium/AlertHandles.java
@@ -1,4 +1,5 @@
 package learnSelenium;
+
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.Alert;
@@ -9,16 +10,16 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class AlertHandles {
 	public static void main(String[] args) {
-//		System.setProperty("webdriver.chrome.driver", "./Exe/chromedriver.exe ");
-//		System.setProperty("webdriver.gecko.driver", "./Exe/geckodriver.exe ");
-//		WebDriver driver=new ChromeDriver();
-		WebDriver driver=new FirefoxDriver();
+		// System.setProperty("webdriver.chrome.driver", "./Exe/chromedriver.exe ");
+		// System.setProperty("webdriver.gecko.driver", "./Exe/geckodriver.exe ");
+		// WebDriver driver=new ChromeDriver();
+		WebDriver driver = new FirefoxDriver();
 		driver.get("https://www.irctc.co.in");
 		driver.manage().timeouts().implicitlyWait(5000, TimeUnit.SECONDS);
 		driver.findElement(By.id("loginbutton")).click();
-		Alert alert=driver.switchTo().alert();
+		Alert alert = driver.switchTo().alert();
 		System.out.println(alert.getText());
 		alert.accept();
 		driver.quit();
 	}
-	}
+}
